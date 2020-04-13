1602791fc1838f2cbf4d2fe0ac53d7858b3357e5
dnbaker <dnb@cs.jhu.edu>
2020-04-13 14:13:54 -0400

    Single include header.

---
+++ fastiota_ho.h
@@ -0,0 +1,9 @@
+#ifndef FAST_IOTA_HO_H_
+#define FAST_IOTA_HO_H_
+
+#ifndef SINGLE_HEADER_FAST_IOTA
+#  define SINGLE_HEADER_FAST_IOTA 1
+#endif
+#include "fastiota.h"
+
+#endif /* FAST_IOTA_HO_H_ */
